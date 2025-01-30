e4f62ce49ba6e5ca8b2df22085e769085df7498d
AzizBenIsmail <aziz.270700@gmail.com>
2025-01-31 00:55:14 +0100

    sécuriser les route
    sécuriser les route en fonction de token sans session

---
+++ middleware/authMiddlewares.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel');
+
+const requireAuthUser = async (req, res, next) => {
+    const token = req.cookies.jwt_token;
+    console.log('jwt token', token);
+
+    if(token) {
+
+        jwt.verify(token,"net DAR BLOCKCHAIN secret",async (err, decodedToken) => {
+
+            if(err) {
+                res.status(401).json("problem decoding token")
+            }else {
+                console.log("decodedToken",decodedToken)
+                console.log("decodedToken id ",decodedToken.id)
+
+                    //Sans session pour le moment  
+                user = await userModel.findById(decodedToken.id)
+                console.log("user", user)
+                next();
+            }
+
+        });
+    
+    } else {
+        res.status(401).json("/pas de token / il faut etre conn")
+    }
+}
+
+module.exports = {requireAuthUser};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
+const {requireAuthUser} = require('../middleware/authMiddlewares');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.status(200).json('testUsers mrigla');
 });
 
-router.get('/getAllUsers',userController.getAllUsers );
+router.get('/getAllUsers',requireAuthUser,userController.getAllUsers );
 router.post('/register',userController.register );
 router.post('/login',userController.login );
 
