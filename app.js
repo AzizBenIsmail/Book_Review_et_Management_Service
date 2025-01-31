3a59c1f08f7eaeb28c42715b142b498f22a0bb2c
AzizBenIsmail <aziz.270700@gmail.com>
2025-01-31 01:01:07 +0100

    fix bug

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ const session = require('express-session');
 
 require('dotenv').config();
 
-app.use(session({
-  secret: 'net DAR BLOCKCHAIN secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie:{
-    secure: false, // À définir sur true si vous utilisez HTTPS
-    maxAge: 2 * 60 * 60,
-  }
-}))
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/usersRouter");
@@ -29,6 +21,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+  secret: 'net DAR BLOCKCHAIN secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    secure: false, // À définir sur true si vous utilisez HTTPS
+    maxAge: 2 * 60 * 60,
+  }
+}))
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
